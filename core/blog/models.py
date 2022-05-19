a2af572cdae0a4a7a26cec6efc84b620f2448a6b
salar403 <salar40340@gmail.com>
2022-05-19 23:08:18 +0430

    tiny improvement

--- core/blog/models.py
+++ core/blog/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 
 
 class UsersAdditionalInfo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="info")
     bio = models.CharField(max_length=500, null=True, blank=True)
     profile_picture = models.ImageField()
 
 
 class Posts(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     title = models.CharField(max_length=300)
     content = models.TextField()
     like = models.IntegerField(default=0)
