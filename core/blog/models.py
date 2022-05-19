17cd728d194af94c44ce5bc55fbcb1b5224800fc
torham <torhamdev@gmail.com>
2022-05-19 22:31:30 +0430

    Add basic of user and posts models

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UsersAdditionalInfo(models.Model):
+    user = models.ForeignKey(User)
+    bio = models.CharField(max_length=500, null=True, blank=True)
+    profile_picture = models.ImageField()
+
+
+class Posts(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
